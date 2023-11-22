6c3d98b4504758e85e508cf8e02ec68ad32eea29
igaeun <igaeun0112@gmail.com>
2023-11-22 20:02:58 +0900

    Input-Data 렌더링

--- src/App.js
+++ src/App.js
@@ -10,12 +10,47 @@ const AppWrap = styled.div`
     left: 50%;
     top: 50%;
     position: absolute;
+    transform: translate(-50%, -50%);
+    padding: 20px;
+
+    .cityInput {
+      padding: 12px;
+      border-radius: 12px;
+      font-size: 15px;
+    }
+  }
+`
+
+const WeatherWrap = styled.div`
+  margin-top: 30px;
+  padding: 12px;
+  border: 2px black solid;
+  border-radius: 12px;
+  display: flex;
+  flex-direction: column;
+
+  .cityName {
+    font-size: 20px;
+    font-weight: 400;
+  }
+
+  .temp {
+    font-size: 40px;
+    font-weight: 600;
+    text-align: center;
+  }
+
+  .sky {
+    font-size: 20px;
+    font-weight: 400;
+    text-align: right;
   }
 `
 
 function App() {
   const API_KEY = "08e41be9335d813cf0c2fff93b748157";
   const [location, setLocation] = useState(''); // 빈 문자열로 초기화
+  const [result, setResult] = useState({});
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
 
   // 비동기
@@ -27,6 +62,7 @@ function App() {
           url: url
         })
         console.log(data);
+        setResult(data);
       }
       catch (err) {
         alert(err);
@@ -38,7 +74,8 @@ function App() {
   return (
     <AppWrap>
       <div className='contentWrap'>
-        <input 
+        <input
+          className='cityInput'
           placeholder='도시를 입력하세요'
           value={location}
           // input 값이 변할 때마다, setLocation 변경 
@@ -46,7 +83,13 @@ function App() {
           type='text'
           onKeyDown={searchWeather}
         />
-
+        {Object.keys(result).length !== 0 && (
+           <WeatherWrap>
+           <div className='cityName'>{result.data.name}</div>
+           <div className='temp'>{result.data.main.temp}</div>
+           <div className='sky'>{result.data.weather[0].main}</div>
+         </WeatherWrap>
+        )}
       </div>
     </AppWrap>
   );
